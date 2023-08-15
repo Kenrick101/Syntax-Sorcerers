0661784a40afe6f6450051ff7b833958cb5aa2e5
KyleTW0409 <139485685+KyleTW0409@users.noreply.github.com>
2023-08-15 13:32:06 +0200

    Everythings changed

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeCss () {
+    var navElement = document.querySelector(".nav-bar");
+    if(this.scrollY > 1)
+    {
+     
+      navElement.style.backgroundColor = "black";
+    }
+    else
+    {
+       navElement.style.backgroundColor = "";
+    }
+ }
+ 
+   window.addEventListener("scroll", changeCss , false);
